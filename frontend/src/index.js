f25e80031820ae85292de9c65939c79dce1330ca
Cameron Smith <CamSmith@CamSmith.co.nz>
2025-03-05 09:07:29 +1300

    auth keys for supabase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './styles/main.css'; // Import our new main styles
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { MockAuthProvider } from './context/MockAuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <MockAuthProvider>
-        <App />
-      </MockAuthProvider>
+      <App />
     </Router>
   </React.StrictMode>
 );
