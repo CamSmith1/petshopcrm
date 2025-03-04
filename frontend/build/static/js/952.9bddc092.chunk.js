e24f6fd318f0417075b542071228be99700a4ed9
Cameron Smith <CamSmith@CamSmith.co.nz>
2025-03-05 09:16:51 +1300

    fix authentication issues

---
+++ frontend/build/static/js/952.9bddc092.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbooking_pro_frontend=self.webpackChunkbooking_pro_frontend||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.9bddc092.chunk.js.map
